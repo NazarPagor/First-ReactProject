4030875dfe9583ecb0b355bb217691fb3991e630
Назар Пагор <pagor.n2003@gmail.com>
2021-04-16 21:17:16 +0300

    releas v1

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://api.freelogodesign.org/assets/thumb/logo/22073155_400.png" alt="foto"/>
+        </header>
+    )
+}
+export default Header;
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav className="nav">
+            <ul>
+                <li className="link">Profile</li>
+                <li className="link">Meseage</li>
+                <li className="link">News</li>
+                <li className="link">Music</li>
+            </ul>
+        </nav>
+    )
+}
+export default Nav;
