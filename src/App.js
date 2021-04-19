5f114f461e3f5832eb5007537111e60d0d1f6a4c
Назар Пагор <pagor.n2003@gmail.com>
2021-04-19 17:28:34 +0300

    root-перходи-маршрутизація

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, {Component} from "react";
+import React from "react";
 import './App.css';
 import Header from "./Components/Header/Header";
 import Nav from "./Components/Nav/Nav";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
+import {BrowserRouter} from "react-router-dom";
+import {Route} from "react-router";
+import News from "./Components/News/News";
+import Music from "./Components/Music/Music";
 
 
 const App = () => {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Nav/>
-            <div className="app-wrapper-content">
-                <Dialogs/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Nav/>
+                <div className="app-wrapper-content">
+                    <Route path="/message" component={Dialogs}/>
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/news" component={News} />
+                    <Route path="/music" component={Music} />
+                </div>
             </div>
-           {/* <Profile/>*/}
-        </div>
+        </BrowserRouter>
     )
 }
 
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import classe from "./Nav.module.css";
+
 const Nav = () => {
     return (
         <nav className={classe.nav}>
             <ul>
-                <li className={classe.link}>Profile</li>
-                <li className={classe.link}>Profile</li>
-                <li className={classe.link}>Meseage</li>
-                <li className={classe.link}>News</li>
-                <li className={classe.link}>Music</li>
+                <a href="/profile">
+                    <li className={`${classe.link} ${classe.activ}`}>Profile</li>
+                </a>
+                <a href="/message">
+                    <li className={classe.link}>Meseage</li>
+                </a>
+                <a href="/news">
+                    <li className={classe.link}>News</li>
+                </a>
+                <a href="/music">
+                    <li className={classe.link}>Music</li>
+                </a>
             </ul>
         </nav>
     )
